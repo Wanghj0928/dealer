96d0f86279cc06b15fa4d5977f4af9dde37be064
wanghj <2947713750@qq.com>
2018-01-05 16:31:59 +0800

    新建springboot项目

---
+++ src/main/java/com/wanghj/dealer/DealerApplication.java
@@ -0,0 +1,12 @@
+package com.wanghj.dealer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealerApplication.class, args);
+	}
+}
